1a309aa9ef2e87dc6325a36e97f2bb30201556f6
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 01:01:15 +0200

    Fix New resource app_service

--- main.tf
+++ main.tf
@@ -6,14 +6,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.ENV}-${var.resource_group_name}"
   location = var.resource_group_location
 }
-
-resource "azurerm_app_service_plan" "app_service_plan" {
+resource "azurerm_service_plan" "app_service_plan" {
   name                = "${var.ENV}-${var.app_service_plan_name}"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  kind                = "linux"
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
+  os_type             = "Linux"
+  sku_name            = "FREE"
 }
