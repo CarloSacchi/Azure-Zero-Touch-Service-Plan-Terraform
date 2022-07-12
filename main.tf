488de21963c3d391fe93ff55a3312da456cd78ad
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 10:17:25 +0200

    f1

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "FREE"
+  sku_name            = "F1"
   depends_on = [
     azurerm_resource_group.rg
   ]
