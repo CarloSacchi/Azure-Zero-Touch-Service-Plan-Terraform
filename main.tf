4cad8e2d83c84e8aaf8cf716516ade9222f6136d
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 09:27:04 +0200

    test depends on

--- main.tf
+++ main.tf
@@ -13,5 +13,7 @@ resource "azurerm_service_plan" "app_service_plan" {
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name            = "FREE"
-  depends_on          = [data.azurerm_resource_group.rm]
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
