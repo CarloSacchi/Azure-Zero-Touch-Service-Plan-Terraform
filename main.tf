4c1e5a534524421bed6bb1a054ecd135db195729
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 12:00:35 +0200

    fix app_service_plan

--- main.tf
+++ main.tf
@@ -21,6 +21,9 @@ resource "azurerm_linux_web_app" "app_service" {
   name                = "${var.ENV}-${var.azurerm_app_service_name}"
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_service_plan_name.app_service_plan.id
+  service_plan_id     = azurerm_service_plan.app_service_plan.id
   site_config {}
+  depends_on = [
+    azurerm_service_plan.app_service_plan
+  ]
 }
