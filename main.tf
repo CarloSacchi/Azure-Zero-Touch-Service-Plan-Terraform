b3ad602208468ee23f2de05454d89ce8ceddf864
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 14:07:50 +0200

    fix configuration

--- main.tf
+++ main.tf
@@ -18,12 +18,16 @@ resource "azurerm_service_plan" "app_service_plan" {
   ]
 }
 resource "azurerm_app_service" "app_service" {
-  name                      = "${var.ENV}-${var.azurerm_app_service_name}"
-  location                  = var.resource_group_location
-  resource_group_name       = azurerm_resource_group.rg.name
-  app_service_plan_id           = azurerm_service_plan.app_service_plan.id
+  name                = "${var.ENV}-${var.azurerm_app_service_name}"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_service_plan.app_service_plan.id
   site_config {
-      use_32_bit_worker_process = true
+    use_32_bit_worker_process = true
+    php_version               = "7.4"
+    ftps_state                = "Disabled"
+    http2_enabled             = "true"
+    min_tls_version           = "1.2"
   }
 
 }
