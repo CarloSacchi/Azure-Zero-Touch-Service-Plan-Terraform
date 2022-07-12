08a89a9cdad0455dc73bc1dad0f9d3623fca5899
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 12:10:49 +0200

    use_32_bit_worker_process

--- main.tf
+++ main.tf
@@ -22,5 +22,7 @@ resource "azurerm_linux_web_app" "app_service" {
   location            = var.resource_group_location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.app_service_plan.id
-  site_config {}
+  site_config {
+    use_32_bit_worker_process = true
+  }
 }
