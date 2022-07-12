a9ed0405a3a1b922dcfa31b74f025dd019089cdc
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 09:37:13 +0200

    provider to from 3.9 to 3.13

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.9.0"
+      version = "3.13.0"
     }
   }
   required_version = ">= 0.14"
