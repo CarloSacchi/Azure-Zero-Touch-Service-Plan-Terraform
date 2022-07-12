1815dbaabf332784695106e20c03203e5f35fc0d
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 11:55:44 +0200

    fix service_plan_id

--- main.tf
+++ main.tf
@@ -18,9 +18,9 @@ resource "azurerm_service_plan" "app_service_plan" {
   ]
 }
 resource "azurerm_linux_web_app" "app_service" {
-  name                      = "${var.ENV}-${var.azurerm_app_service_name}"
-  location                  = var.resource_group_location
-  resource_group_name       = azurerm_resource_group.rg.name
-  azurerm_service_plan_name = azurerm_service_plan_name.app_service_plan.name
+  name                = "${var.ENV}-${var.azurerm_app_service_name}"
+  location            = var.resource_group_location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan_name.app_service_plan.id
   site_config {}
 }
