7da916dcb6ee2b7da66f99b0ac79cedcdb450708
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 13:59:08 +0200

    fix stuff

--- main.tf
+++ main.tf
@@ -17,16 +17,13 @@ resource "azurerm_service_plan" "app_service_plan" {
     azurerm_resource_group.rg
   ]
 }
-resource "azurerm_linux_web_app" "app_service" {
+resource "azurerm_app_service" "app_service" {
   name                      = "${var.ENV}-${var.azurerm_app_service_name}"
   location                  = var.resource_group_location
   resource_group_name       = azurerm_resource_group.rg.name
-  service_plan_id           = azurerm_service_plan.app_service_plan.id
+  app_service_plan_id           = azurerm_service_plan.app_service_plan.id
   site_config {
-      use_32_bit_worker = true
-        application_stack{
-      php_version = "8.0"
-  }
+      use_32_bit_worker_process = true
   }
 
 }
--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "azurerm_linux_web_app" {
-  value = azurerm_linux_web_app.app_service.name
+output "azurerm_app_service" {
+  value = azurerm_app_service.app_service.name
 }
