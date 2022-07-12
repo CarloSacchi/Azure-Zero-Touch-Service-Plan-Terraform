6d0bca7a8d25da8e1c7c7c0f2737b23b950d5cb3
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 09:12:02 +0200

    Added depends on azure asp

--- main.tf
+++ main.tf
@@ -6,10 +6,12 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.ENV}-${var.resource_group_name}"
   location = var.resource_group_location
 }
+
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "${var.ENV}-${var.app_service_plan_name}"
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   os_type             = "Linux"
   sku_name            = "FREE"
+  depends_on          = [azurerm_resource_group.rm]
 }
