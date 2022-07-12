0173292711a56fb85326e1753f76c0e8cd2c8cbe
Carlo Sacchi <sacchi.carlo@gmail.com>
2022-07-12 14:59:59 +0200

    fix output

--- output.tf
+++ output.tf
@@ -2,6 +2,6 @@ output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "azurerm_app_service" {
-  value = azurerm_app_service.app_service.name
+output "azurerm_app_service_name" {
+    value = "https://${azurerm_app_service.app_service.name}.azurewebsites.net"
 }
